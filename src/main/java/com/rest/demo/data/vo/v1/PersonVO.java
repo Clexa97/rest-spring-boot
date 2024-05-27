48052f9ff7cf7344e6eac0cc0636027c90c96406
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-05-27 09:50:17 -0300

    Implementando ordem

--- src/main/java/com/rest/demo/data/vo/v1/PersonVO.java
+++ src/main/java/com/rest/demo/data/vo/v1/PersonVO.java
@@ -1,9 +1,11 @@
 package com.rest.demo.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 
 import java.io.Serializable;
 
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter @EqualsAndHashCode
 public class PersonVO implements Serializable {
     private Long id;
