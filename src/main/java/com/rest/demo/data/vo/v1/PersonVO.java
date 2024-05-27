215a87eeb173a10a8a689b3c1341dcf614d27a78
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-05-27 10:28:51 -0300

    Content Negotiation

--- src/main/java/com/rest/demo/data/vo/v1/PersonVO.java
+++ src/main/java/com/rest/demo/data/vo/v1/PersonVO.java
@@ -1,5 +1,7 @@
 package com.rest.demo.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 
@@ -9,8 +11,11 @@ import java.io.Serializable;
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter @EqualsAndHashCode
 public class PersonVO implements Serializable {
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
+    @JsonIgnore
     private String gender;
 }
